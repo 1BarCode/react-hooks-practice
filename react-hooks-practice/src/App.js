52ba72b534dfc33adfe1e6622ba76a24542ba97f
1BarCode <69029687+1BarCode@users.noreply.github.com>
2021-01-24 15:27:53 -0800

    spliting different state into many useState
    use different useState hooks for different states

--- react-hooks-practice/src/App.js
+++ react-hooks-practice/src/App.js
@@ -3,19 +3,23 @@ import React, { useState}  from 'react'
 
 function App() {
   const [count, setCount] = useState(4)
+  const [theme, setTheme] = useState('blue')
 
   function decrement(){
     setCount(prevCount => prevCount - 1)
+    setTheme('green')
   }
 
   function increment(){
     setCount(prevCount => prevCount + 1)
+    setTheme('red')
   }
 
   return (
     <div>
       <button onClick={decrement}>-</button>
       <span>{count}</span>
+      <span>{theme}</span>
       <button onClick={increment}>+</button>
     </div>
   )
