cdd42203486afe593a1b157f0fa0553c2ca423f9
1BarCode <69029687+1BarCode@users.noreply.github.com>
2021-01-24 14:54:25 -0800

    created state with object
    need to spread prevState when updating

--- react-hooks-practice/src/App.js
+++ react-hooks-practice/src/App.js
@@ -1,11 +1,29 @@
-import React from 'react'
+import React, { useState} from 'react'
 
 function App() {
+  // for complex state use useState(() => 4)
+  const [state, setState] = useState({ count: 4, theme: 'blue'})
+  const count = state.count
+  const theme = state.theme
+
+  function decrement() {
+    setState(prevState => {
+      return {...prevState, count: prevState.count - 1 }
+    })
+  }
+
+  function increment() {
+    setState(prevState => {
+      return {count: prevState.count + 1}
+    })
+  }
+
   return (
     <div>
-      <button>-</button>
-      <span>0</span>
-      <button>+</button>
+      <button onClick={decrement}>-</button>
+      <span>{count}</span>
+      <span>{theme}</span>
+      <button onClick={increment}>+</button>
     </div>
   )
 }
